f19bbb90fa61e78f2b4d618d6e4fb98e3ea53985
Sourabh <bhairavkar.sourabh@gmail.com>
2020-07-24 22:57:44 +0530

    Adding load timeout

--- SeleniumTrial.py
+++ SeleniumTrial.py
@@ -4,19 +4,20 @@ import time
 
 #driver = webdriver.Ie("C:\\Users\\sb30578\\JD\\Non Project\\Study Material\\Selenium\\IE Drivers\\IEDriverServer.exe")
 
-driver = webdriver.Chrome("C:\\Users\\sb30578\\JD\\Non Project\\Study Material\\Selenium\\Chrome Drivers\\chromedriver.exe")
+driver = webdriver.Chrome("C:\\Users\\owner\\PycharmProjects\\SeleniumTutorial\\Chrome Driver\\chromedriver.exe")
 
 driver.set_page_load_timeout(5)
 
 driver.get("https://www.linkedin.com/")
 driver.set_page_load_timeout(5)
 
-driver.find_element_by_class_name("nav__button-secondary")
+driver.find_element_by_class_name("nav__button-secondary").click()
+driver.set_page_load_timeout(100)
 driver.find_element_by_name("session_key").send_keys("bhairavkar.sourabh@gmail.com")
+driver.set_page_load_timeout(100)
+driver.find_element_by_name("session_password").send_keys("LinSoubhai300")
+driver.set_page_load_timeout(100)
+driver.find_element_by_css_selector("btn__primary--large from__button--floating").send_keys(Keys.ENTER)
 
-driver.find_element_by_name("session_password").send_keys("LinSoubhai@300")
 
-driver.find_element_by_name("homepage-basic_signin-form_submit-button").send_keys("bhairavkar.sourabh@gmail.com")
-
-driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(5)
