9c4f0a016e26b58980ea6397cfe712cddae72248
Sourabh <bhairavkar.sourabh@gmail.com>
2020-07-25 22:09:32 +0530

    Adding load timeout

--- SeleniumTrial.py
+++ SeleniumTrial.py
@@ -1,23 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
 #driver = webdriver.Ie("C:\\Users\\sb30578\\JD\\Non Project\\Study Material\\Selenium\\IE Drivers\\IEDriverServer.exe")
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome("C:\\Users\\owner\\PycharmProjects\\SeleniumTutorial\\Chrome Driver\\chromedriver.exe")
 
-driver.set_page_load_timeout(5)
+#driver.implicitly_wait(10000)
 
-driver.get("https://www.linkedin.com/")
-driver.set_page_load_timeout(5)
+driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 
-driver.find_element_by_class_name("nav__button-secondary").click()
-driver.set_page_load_timeout(100)
+
+#driver.find_element_by_class_name("nav__button-secondary").click()
 driver.find_element_by_name("session_key").send_keys("bhairavkar.sourabh@gmail.com")
-driver.set_page_load_timeout(100)
-driver.find_element_by_name("session_password").send_keys("LinSoubhai300")
-driver.set_page_load_timeout(100)
-driver.find_element_by_css_selector("btn__primary--large from__button--floating").send_keys(Keys.ENTER)
+driver.find_element_by_name("session_password").send_keys("")
+driver.find_element_by_xpath("//*[@id='app__container']/main/div[2]/form/div[3]/button").click()
 
+class_name = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='ember41']/input")))
 
-time.sleep(5)
+class_name.send_keys("Archana Gaikwad")
+class_name.send_keys(Keys.ENTER)
+
+message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "msg-overlay-list-bubble-search__search-typeahead-input")))
+
+message.send_keys("Archana Gaikwad")
+
